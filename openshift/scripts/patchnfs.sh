48de0f46e9f53608dea441d04ab2ebc597604091
Filipe Caló <filipecm@gmail.com>
2026-03-11 17:35:09 -0300

    nfs

--- openshift/scripts/patchnfs.sh
+++ openshift/scripts/patchnfs.sh
@@ -5,3 +5,9 @@ oc patch storageclass nfs-csi --type=json -p '[{"op":"add","path":"/mountOptions
 for pv in $(oc get pv -o json | jq -r '.items[] | select(.spec.storageClassName=="nfs-csi") | .metadata.name'); do
   oc patch pv $pv --type merge -p '{"spec":{"mountOptions":["nolock","nfsvers=4.1","hard","intr"]}}'
 done
+
+
+
+semanage fcontext -a -t nfs_t "/data/ds1(/.*)?"
+semanage fcontext -a -t nfs_t "/data/ds2(/.*)?"
+restorecon -Rv /data/ds1 /data/ds2
